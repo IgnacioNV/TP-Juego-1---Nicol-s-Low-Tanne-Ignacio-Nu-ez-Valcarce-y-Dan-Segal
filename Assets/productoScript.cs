8d4d65932e2dd9935c02669217dc93607867cac2
IgnacioNV <ignacionunezvalcarce@gmail.com>
2024-06-24 17:35:17 -0300

    u

---
+++ Assets/productoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class productoScript : MonoBehaviour
+{
+    public int precio;
+    public GameObject producto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

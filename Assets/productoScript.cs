702536d0b6d083b5988cf55edde0fe644c2b37d0
Ignacio Nuñez Valcarce <ignacionunezvalcarce@gmail.com>
2024-06-25 10:43:36 -0300

    u

--- Assets/productoScript.cs
+++ Assets/productoScript.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class productoScript : MonoBehaviour
 {
-    public int precio;
-    public GameObject producto;
-
+    public GameObject[] arrayProducto;
+    Vector3 positionProduct1 = new Vector3(-3.77f, 0.8f, -0.1f);
+    Vector3 positionProduct2 = new Vector3(3.89f, 0.8f, -0.1f);
     // Start is called before the first frame update
     void Start()
     {
-        
+        crearRandom1(positionProduct1);
+        crearRandom1(positionProduct2);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void crearRandom1(Vector3 position)
+    {
+        int aleatorio = Random.Range(0, arrayProducto.Length - 1);
+        arrayProducto[aleatorio].SetActive(true);
+        arrayProducto[aleatorio].transform.position = position;
     }
 }

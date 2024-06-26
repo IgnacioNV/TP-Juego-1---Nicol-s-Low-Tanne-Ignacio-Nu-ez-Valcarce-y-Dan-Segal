434de69f3a16b5eb0b11ca2316e5b32869d2782b
ncapolt <nicolaslowtanne@gmail.com>
2024-06-25 22:56:05 -0300

    u

--- Assets/Scripts/productoScript.cs
+++ Assets/Scripts/productoScript.cs
@@ -12,9 +12,22 @@ public class productoScript : MonoBehaviour
     public Text precioProducto1;
     public Text precioProducto2;
     public InputField inputPrecio;
-
+    public float suma;
+    public Text textoNotificacion;
     // Start is called before the first frame update
     void Start()
+    {
+        SecuenciaObjetoRandom();
+        textoNotificacion.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SecuenciaObjetoRandom()
+
     {
         Debug.Log("The game has started!");
         foreach (GameObject producto in arrayProducto)
@@ -26,11 +39,8 @@ public class productoScript : MonoBehaviour
 
         precioProducto1.text = "$" + producto1.GetComponent<precioScript>().precio.ToString();
         precioProducto2.text = "$" + producto2.GetComponent<precioScript>().precio.ToString();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        suma = producto1.GetComponent<precioScript>().precio + producto2.GetComponent<precioScript>().precio;
 
     }
 
@@ -40,6 +50,44 @@ public class productoScript : MonoBehaviour
         arrayProducto[aleatorio].SetActive(true);
         arrayProducto[aleatorio].transform.position = position;
         return arrayProducto[aleatorio];
+    }
+
+
+
+    //
+    public void ValidarSuma()
+    {
+        if (string.IsNullOrEmpty(inputPrecio.text))
+        {
+            MostrarNotificacion("Debes ingresar un resultado");
+        }
+        else
+        {
+            float valorIngresado;
+            bool esNumero = float.TryParse(inputPrecio.text, out valorIngresado);
+
+            if (esNumero)
+            {
+                if (valorIngresado == suma)
+                {
+                    MostrarNotificacion("Resultado correcto");
+                }
+                else
+                {
+                    MostrarNotificacion("Resultado incorrecto");
+                }
+            }
+            else
+            {
+                MostrarNotificacion("Ingresa un valor numérico válido");
+            }
+        }
+    }
+
+    // Método para mostrar notificaciones
+    void MostrarNotificacion(string mensaje)
+    {
+        textoNotificacion.text = mensaje;
     }
 }
 
